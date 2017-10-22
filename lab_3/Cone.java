dcf9076b519e1e4f0017f7d3ea159818f24c99ca
Evgeniy <32490926+cadencedhaunter@users.noreply.github.com>
2017-10-22 20:15:22 +0300

    Add files via upload

---
+++ lab_3/Cone.java
@@ -0,0 +1,47 @@
+package lab_3;
+
+import java.io.Serializable;
+
+public class Cone implements Serializable {
+	 
+	private float baseRadius;
+	private float height;
+	
+	
+	private static final double PI = 3.1415;
+	
+	Cone(){}
+	
+	Cone(float baseRadius, float height){
+		this.baseRadius = baseRadius;
+		this.height = height;
+	}
+	
+	public double getArea() {
+		double hypotenuse = Math.sqrt((height*height) + (baseRadius*baseRadius));
+		return (PI * baseRadius * (baseRadius + hypotenuse));
+	}
+	
+	
+	public void setHeight(float height) {
+		this.height = height;
+	}
+	
+	public void setBaseRadius(float radius) {
+		baseRadius = radius;
+	}
+	
+	public float getBaseRadius() {
+		return baseRadius;
+	}
+	
+	public float getHeight() {
+		return height;
+	}
+	
+	public String toString() {
+		return "Area of cone with radius: " + baseRadius +
+			   "; height: " + height + 
+			   "; is: " + getArea();
+	}
+}
---
+++ lab_3/Runner.java
@@ -0,0 +1,55 @@
+package lab_3;
+
+import java.util.Scanner;
+import java.io.*;
+
+public class Runner {
+	
+	public static void main(String[] args) {
+
+		Cone cone = new Cone();
+		
+		Scanner scan = new Scanner(System.in);
+		
+		cone.setBaseRadius(scan.nextFloat());
+		cone.setHeight(scan.nextFloat());
+
+
+		System.out.println("Cone to write:\n" + cone);
+		
+		File file = new File("C:\\del\\test.txt");
+		ObjectOutputStream writeStream = null;
+		
+		try {
+			writeStream = new ObjectOutputStream(new FileOutputStream(file));
+			writeStream.writeObject(cone);
+		} catch (Exception e) {
+		} finally {
+			try {
+				if (writeStream!=null){writeStream.close();}
+			}catch (IOException e) {
+				e.printStackTrace(System.err);
+			}
+		}
+		
+		ObjectInputStream readStream = null;
+		Cone readCone = new Cone();
+		
+		try {
+			readStream = new ObjectInputStream(new FileInputStream(file));
+			readCone = (Cone) readStream.readObject();
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+		} finally {
+			try {
+				if (readStream!=null){readStream.close();}
+			} catch (Exception e) {
+				e.printStackTrace(System.err);
+			}
+		}
+		
+		System.out.println("Readed cone:\n"+readCone);
+		
+		scan.close();
+	}
+}
